167ee15b59b25dff9dd29280644e51928d9a6b8b
Ismael Vayra <ismael.vayra@gmail.com>
2018-04-02 20:25:43 -0300

    fixes some dagger thing

--- app/src/main/java/com/template/clean/cleanarchitecturetemplate/dagger/components/MainComponent.java
+++ app/src/main/java/com/template/clean/cleanarchitecturetemplate/dagger/components/MainComponent.java
@@ -5,6 +5,7 @@ import com.template.clean.cleanarchitecturetemplate.dagger.modules.MainModule;
 
 import javax.inject.Singleton;
 
+import dagger.BindsInstance;
 import dagger.Component;
 import dagger.android.AndroidInjectionModule;
 import dagger.android.AndroidInjector;
@@ -14,7 +15,16 @@ import dagger.android.AndroidInjector;
  */
 
 @Singleton
-@Component(modules = { AndroidInjectionModule.class, MainModule.class })
+@Component(modules = {
+        AndroidInjectionModule.class,
+        MainModule.class,
+})
 public interface MainComponent extends AndroidInjector<CleanArchApplication> {
-
+    @Component.Builder
+    interface Builder {
+        @BindsInstance
+        Builder application(CleanArchApplication application);
+        MainComponent build();
+    }
+    void inject(CleanArchApplication app);
 }
